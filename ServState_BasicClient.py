3ebd76c8cd07550f33dd17422aa3d7e1abcbb0f4
Rose Awen Brindle <rosebrindle@outlook.com>
2021-09-20 01:01:31 +0100

    basic client-server comms complete

--- ServState_BasicClient.py
+++ ServState_BasicClient.py
@@ -1,36 +1,86 @@
 # ServState TCP Client: C:/Users/roseb/AppData/Local/Programs/Python/Python39/python.exe "f:/Documents/- Code -/ServState/ServState_BasicClient.py"
+#                       
 import socket
 import threading
 from time import sleep
+import time
 import json
 import psutil
+import signal
+import sys
+
+REPORT_RATE = 2 * 10**9      # nanoseconds between status reports
+DATA_UPDATE_RATE = 10        # milliseconds between data updates
 
 LOCK = threading.Lock()
 
-REPORTDATA = {"cpu_usage": True, "cpu_freq": True, "disk1_usage": True, "disk2_usage": False, "disk3_usage": False,"disk4_usage": False, "boot_time": True}
-current_data = {"cpu_usage": 0, "cpu_freq": 0, "disk1_usage": 0, "disk2_usage": 0, "disk3_usage": 0,"disk4_usage": 0, "boot_time": 0}
+# leave blank if not reporting that disk's stats
+DISK1_ADDR = "C:/"
+DISK2_ADDR = ""
+DISK3_ADDR = ""
+DISK4_ADDR = ""
 
+REPORTDATA = {"cpu_usage": True, "cpu_freq": True, "disk1_usage": True, "disk2_usage": False, "disk3_usage": False,"disk4_usage": False, "up_time": True}
+current_data = {"epoch_time": 0, "cpu_usage": 0, "cpu_freq": 0, "disk1_usage": 0, "disk2_usage": 0, "disk3_usage": 0,"disk4_usage": 0, "up_time": 0}
+
+runStatsUpdate = True
 def updateStats():
-    while True:
-        data = psutil.cpu_percent(interval=1)
+    while runStatsUpdate:
+        epoch_time = time.time_ns();
+        if REPORTDATA["cpu_usage"]: cpu_usage = psutil.cpu_percent(interval=1)
+        if REPORTDATA["cpu_freq"]: cpu_freq = psutil.cpu_freq().current
+        if REPORTDATA["disk1_usage"]: disk1_usage = psutil.disk_usage(DISK1_ADDR).percent
+        if REPORTDATA["disk2_usage"]: disk2_usage = psutil.disk_usage(DISK2_ADDR).percent
+        if REPORTDATA["disk3_usage"]: disk3_usage = psutil.disk_usage(DISK3_ADDR).percent
+        if REPORTDATA["disk4_usage"]: disk4_usage = psutil.disk_usage(DISK4_ADDR).percent
+        if REPORTDATA["up_time"]: up_time =  epoch_time - (psutil.boot_time() * 10**9)
+        
         with LOCK:
-            current_data["cpu_usage"] = data
+            current_data["epoch_time"] = epoch_time
+            if REPORTDATA["cpu_freq"]: current_data["cpu_freq"] = cpu_freq
+            if REPORTDATA["cpu_usage"]: current_data["cpu_usage"] = cpu_usage
+            if REPORTDATA["disk1_usage"]: current_data["disk1_usage"] = disk1_usage
+            if REPORTDATA["disk2_usage"]: current_data["disk2_usage"] = disk2_usage
+            if REPORTDATA["disk3_usage"]: current_data["disk3_usage"] = disk3_usage
+            if REPORTDATA["disk4_usage"]: current_data["disk4_usage"] = disk4_usage
+            if REPORTDATA["up_time"]: current_data["up_time"] = up_time
+        
+        sleep(DATA_UPDATE_RATE/1000)
             
 
 HOST = socket.gethostname()
 PORT = 2004
 BUFFER_SIZE = 2000 
- 
+
+print("[i] Press ctrl-C to close the connection")
+print("[!] Connecting to server: " + HOST + ':' + str(PORT))
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 client.connect((HOST, PORT))
 
 statsthread = threading.Thread(target=updateStats)
-statsthread.daemon = True
 statsthread.start()
 
-inp = ""
-while inp != 'q':
-    inp = input("enter q to quit or nothing to continue")
+# if ctrl-c sent, tidy then close
+def quit(sig, frame):
+    global runStatsUpdate
+    print("[-] Disconnecting from server")
+    with LOCK:
+        runStatsUpdate = False
+    client.close()
+    statsthread.join()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, quit)
+
+print("[+] Connected to server, reporting every " + str(REPORT_RATE/10**9) + " seconds")
+
+last_run = 0
+while True:
+    if (time.time_ns() - last_run < REPORT_RATE):
+        continue
+
+    last_run = time.time_ns();
 
     with LOCK:
         msg = current_data
@@ -40,9 +90,3 @@ while inp != 'q':
 
     data = client.recv(BUFFER_SIZE)
     print("[r] Client received data:", data)
-
-    sleep(1)
-
-
-statsthread.join()
-client.close()
--- ServState_Server.py
+++ ServState_Server.py
@@ -1,7 +1,13 @@
+# C:/Users/mbrin/AppData/Local/Programs/Python/Python38-32/python.exe "c:/Users/mbrin/Documents/- Code -/ServState/ServState_Server.py"
+
 import socket 
+import threading
 from threading import Thread
-import sys 
+import os, sys
 import json
+import keyboard
+
+LOCK = threading.Lock()
 
 # Multithreaded Python server : TCP Server Socket Thread Pool
 class ClientThread(Thread): 
@@ -9,11 +15,12 @@ class ClientThread(Thread):
     def __init__(self,ip,port): 
         Thread.__init__(self) 
         self.ip = ip 
+        self.end_thread = False
         self.port = port 
         print("[+] Client connected: " + ip + ":" + str(port))
  
-    def run(self): 
-        while True:
+    def run(self):
+        while not self.end_thread:
             try:
                 data = conn.recv(2048)
             except ConnectionAbortedError:
@@ -27,13 +34,15 @@ class ClientThread(Thread):
                 print("[-] Client closed connection: " + ip + ":" + str(port))
                 sys.exit() # end thread
 
-            data_string = data.decode("urf-8")
+            data_string = data.decode("utf-8")
             data_dict = json.loads(data_string)
 
             print("[r] Server received data:", data_dict)
 
             MESSAGE = "RCVD"
             conn.send(bytes(MESSAGE, "utf-8"))
+        
+        conn.close()
 
 
 # Multithreaded Python server : TCP Server Socket Program Stub
@@ -41,18 +50,28 @@ TCP_IP = '0.0.0.0'
 TCP_PORT = 2004 
 BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
 
+print("[iq] Press q to close the server")
 tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 tcpServer.bind((TCP_IP, TCP_PORT)) 
 threads = [] 
+
+# if ctrl-c sent, tidy then close
+def quit():
+    print("[!] Closing connections")
+    for t in threads:
+        t.end_thread = True
+    for t in threads: 
+        print("[-] Waiting for " + t.ip + "to disconnect")
+        t.join() 
+    os._exit(0) # for unkown reasons, sys.exit does not work
+
+keyboard.on_press_key("q", lambda _:quit())
  
 while True: 
     tcpServer.listen(4) 
-    print("[!] Waiting for connections from TCP clients...")
+    print("[!] Searching for connections from TCP clients...")
     (conn, (ip,port)) = tcpServer.accept()
     newthread = ClientThread(ip,port)
     newthread.start()
     threads.append(newthread)
- 
-for t in threads: 
-    t.join() 
